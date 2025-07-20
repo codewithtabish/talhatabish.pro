2eb7010260e4387a3b41c41d849b7eb87acfab65
coderghoust <tabish169000@gmail.com>
2025-07-19 20:29:51 -1200

    fixed paths

--- src/app/[locale]/help/page.tsx
+++ src/app/[locale]/help/page.tsx
@@ -1,6 +1,4 @@
-import AIInputBox from '@/components/custom/(help)/ai-input-box'
 import ChatAssistantComponent from '@/components/custom/(help)/chat-assistant-comp';
-import ChatAssistantUI from '@/components/custom/(help)/chat-assistant-ui'
 import HelpContentComp from '@/components/custom/(help)/help-content-comp';
 import React from 'react'
 
@@ -12,14 +10,9 @@ export default async function HelpPage({
   const { locale } = await params 
   return (
     <div>
-      {/* <AIInputBox/> */}
       {/* @ts-ignore */}
       <HelpContentComp  locale={locale}/>
       <ChatAssistantComponent/>
-      {/* <ChatAssistantUI/> */}
-          {/* ...your help page content */}
-      {/* <VapiWidget /> */}
-      {/* <AssistantButton /> */}
     </div>
   )
 }
